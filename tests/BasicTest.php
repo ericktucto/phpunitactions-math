6254a7e79ec11be87a03537ad7111d12a3b6455b
Erick Tucto <erick@ericktucto.com>
2025-03-24 09:23:47 -0500

    add testing to subtraction

--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -25,4 +25,57 @@ class BasicTest extends TestCase
             $result->float,
         );
     }
+    public function testSumIntegerAndFloat(): void
+    {
+        $a = new Number(5, 8);
+        $b = new Number(6, 9);
+
+        $result = Basic::sum($a, $b);
+
+        $this->assertInstanceOf(Number::class, $result);
+        $this->assertEquals(
+            12,
+            $result->integer,
+        );
+        $this->assertEquals(
+            7,
+            $result->float,
+        );
+    }
+
+    public function testSubOnlyIntegers(): void
+    {
+        $a = new Number(5);
+        $b = new Number(6);
+
+        $result = Basic::sub($b, $a);
+
+        $this->assertInstanceOf(Number::class, $result);
+        $this->assertEquals(
+            1,
+            $result->integer,
+        );
+        $this->assertEquals(
+            0,
+            $result->float,
+        );
+    }
+
+    public function testSubWithIntegerAndFloat(): void
+    {
+        $a = new Number(5, 4);
+        $b = new Number(6, 8);
+
+        $result = Basic::sub($b, $a);
+
+        $this->assertInstanceOf(Number::class, $result);
+        $this->assertEquals(
+            1,
+            $result->integer,
+        );
+        $this->assertEquals(
+            2,
+            $result->float,
+        );
+    }
 }
